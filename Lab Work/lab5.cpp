a6c7c9cb217d650e218371d074572c176b3d467e
sakhi029 <f2025-0991@bnu.edu.pk>
2025-11-25 21:59:19 +0500

    initial commit

--- Lab Work/lab5.cpp
+++ Lab Work/lab5.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
-void menu(int n){
-cout<<"check even/odd,press 1"<<endl;
-cout<<"count digits, press 2"<<endl;
-cout<<"sum of digits,press 3"<<endl;
-cout<<"check prime number,press 4"<<endl;
-cout<<"exit"<<endl;
-int getnumber(int n1)
-}   
-int main(){
-    int num;
-    cout<<"enter number"<<endl;
-    cin>>num;
-    
+int main()
+{
+    string name;
+    std::string password="Python123";
+    do{
+        cout<<"Enter Password"<<endl;
+        cin>>name;
+        if(name==password){
+            break;
+        }else{
+            continue;
+        }
+    }while(true);
+    return 0;
 }
